1ea42191dc091574fad34e439108364f71540b49
= <jkrinsky91@gmail.com>
2026-02-04 17:00:15 -0500

    added function to calculate return metrics

--- analyzer.py
+++ analyzer.py
@@ -9,7 +9,44 @@ import json
 import argparse
 import time
 
-#attempt to pull the daily value over N time. default to one week
+#pull the daily value over N time. default to one week
+def calculate_security_value_metrics(security_data_by_ticker):
+    if not security_data_by_ticker:
+        return None
+    
+    combined_dataframe_for_comparison=pd.concat(security_data_by_ticker,names=["symbol"])
+    comparison_metrics={}#start price, end price,  %change, $change, %return $avg daily return, volatility, risk/reward measurement
+
+    for ticker, security_data in security_data_by_ticker.items():
+        security_data=security_data.sort_values("Date")
+
+        start_price=security_data['4. close'].iloc[0]
+        end_price=security_data['4. close'].iloc[-1]
+        total_return_in_dollars=end_price-start_price
+        total_return_pecentage=((end_price-start_price)/start_price)*100
+        security_data['daily_change']=security_data['4. close'].pct_change()*100
+        avg_daily_percentage_change=security_data['daily_change'].mean()
+        volatility=security_data['daily_change'].std()
+        sharpe_risk_to_reward=avg_daily_percentage_change/volatility
+
+        comparison_metrics[ticker]={
+            'Start Price':start_price,
+            'End Price':end_price,
+            'Total Return($)':total_return_in_dollars,
+            'Total Return(%)':total_return_pecentage,
+            'Average Daily Change(%)':avg_daily_percentage_change,
+            'Volatility(std dev)':volatility,
+            'Risk to Reward(Sharpe Ratio)':sharpe_risk_to_reward,
+
+        }
+    
+    comparison_dataframe=pd.DataFrame(comparison_metrics)
+
+    with open('combined_dataframe.txt', 'w') as outfile:
+        print(comparison_dataframe.to_string(float_format="{:.2f}".format), file=outfile)
+
+    return comparison_dataframe
+
 def get_security_data(symbol, days=7):
     url=f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}&outputsize=compact'
 
@@ -61,14 +98,7 @@ def fetch_multiple_securities(symbols, days=7):
 
     return data_by_symbol, last_refreshed_dates
 
-def compare_securities(security_data_by_ticker):
-    if not security_data_by_ticker:
-        return None
-    
-    combined_dataframe_for_comparison=pd.concat(security_data_by_ticker,names=["symbol"]).reset_index(level="symbol")
 
-    with open('combined_dataframe.txt', 'w') as outfile:
-        print(combined_dataframe_for_comparison.to_string(index=False), file=outfile)
 
 
 #read symbols from cli
@@ -90,7 +120,7 @@ if __name__=="__main__":
 
 
     security_data, last_refresh_dates=fetch_multiple_securities(cli_arguments.symbols, cli_arguments.days)
-    compare_securities(security_data)
+    calculate_security_value_metrics(security_data)
 
     if security_data is not None:
         with open("dataframedata.txt", 'w') as outfile:
